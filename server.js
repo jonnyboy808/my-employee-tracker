56a597620c7d3e03d7c25bd3227fa23652888090
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-29 17:24:00 -0700

    created list for options and created a .then statement for each

--- server.js
+++ server.js
@@ -11,3 +11,41 @@ const db =mysql.createConnection(
     },
     console.log('Connected to the employee_tracker_db database.')
 );
+
+const valueUpdate = () => {
+    inquirer
+     .prompt([
+        {
+            type:'list',
+            name:'choiceUpdate',
+            message:'Choose from the following options.',
+            choices:['View all Employees', 'View Departments', 'Add Department', 'View Roles', 'Add Role', 'Update Employee Role', 'Add Employee', 'Close'
+        ]
+        }
+     ])
+     .then((answer) => {
+        switch(answer.valueUpdate){
+            case 'View all Employees':
+                ViewAllEmployees();
+                break;
+            case 'View Departments':
+                ViewDepartments();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'View Roles':
+                viewRoles();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'Update Employee Role':
+                updateEmployeeRole();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+        }
+     });
+};
